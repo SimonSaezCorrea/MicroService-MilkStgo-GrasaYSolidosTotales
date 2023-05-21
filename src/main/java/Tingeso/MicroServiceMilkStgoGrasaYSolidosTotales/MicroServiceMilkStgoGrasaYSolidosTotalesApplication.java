9c6c3394dea2465ceaf51bf281c3c9da20d4bf6a
Simon Saez <nayxxaxx789super@gmail.com>
2023-05-21 19:09:01 -0400

    Creación de Proyecto IntelliJ

---
+++ src/main/java/Tingeso/MicroServiceMilkStgoGrasaYSolidosTotales/MicroServiceMilkStgoGrasaYSolidosTotalesApplication.java
@@ -0,0 +1,13 @@
+package Tingeso.MicroServiceMilkStgoGrasaYSolidosTotales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceMilkStgoGrasaYSolidosTotalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceMilkStgoGrasaYSolidosTotalesApplication.class, args);
+	}
+
+}
